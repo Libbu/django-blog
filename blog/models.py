880ed3fa2ce8f3d02eed7532d8a73fa8eb9ba162
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-02-22 17:03:16 +0000

    added to post model

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts")
-    content = models.TextField()
+    content = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
