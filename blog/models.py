5fcafc77c398b41aaed14f7511499f4a0cd99003
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-02-22 17:51:34 +0000

    added meta class and title to comments

--- blog/models.py
+++ blog/models.py
@@ -14,8 +14,10 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
+        
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
@@ -28,6 +30,12 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}" 
 
 
 
