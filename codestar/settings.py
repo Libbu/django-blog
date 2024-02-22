5bf03d5ab4e42447733c5591c8ba16f2a42e5914
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-02-22 13:52:16 +0000

    env variables

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pamje*nm#)a_+5a5!4fzzjg7*1yy17&4#*hd+aa4*fcsie+s6n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
